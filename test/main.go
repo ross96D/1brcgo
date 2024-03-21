f4dcf1b36069752927b2d01d4c08007e508717ad
ross96D <abelaross96@gmail.com>
2024-03-20 22:28:26 -0400

    make distribution to match newlines

--- test/main.go
+++ test/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 	"time"
 
 	"github.com/ross96D/mmap"
 )
+
+type CityMetadata struct {
+	Min   int
+	Max   int
+	Sum   int
+	Count int
+}
+
+var cityMap = sync.Map{}
 
 const filename = "../measurements.txt"
 
@@ -25,34 +35,24 @@ func main() {
 		done <- true
 	}()
 
-	var current int64 = 0
-	const threads = 16
+	var threads = runtime.NumCPU()
 	file, err := mmap.Open(filename)
 	if err != nil {
 		panic(err)
 	}
-	info, err := file.Stat()
+	distribution, err := makeDistribution(file, threads)
 	if err != nil {
 		panic(err)
 	}
 
-	var limit int64 = info.Size() / threads
-	for i := 0; i < threads; i++ {
+	for i := 1; i < len(distribution); i++ {
 		wg.Add(1)
 
-		go func(current int64, i int) {
-			var final int64
-			if i == threads-1 {
-				final = info.Size()
-			} else {
-				final = current + limit
-			}
-			read(current, final, file, channel)
+		go func(i int) {
+			read(distribution[i-1], distribution[i], file, channel)
 			fmt.Printf("%d thread has been completed\n", i)
 			wg.Done()
-		}(current, i)
-
-		current += limit
+		}(i)
 	}
 
 	wg.Wait()
@@ -76,6 +76,7 @@ func read(start int64, end int64, file *mmap.File, channel chan int64) {
 	offset := start
 	var bToRead int64
 	for offset < end {
+		// read bytes from file
 		if length+offset > end {
 			bToRead = end - offset
 		} else {
@@ -95,4 +96,45 @@ func read(start int64, end int64, file *mmap.File, channel chan int64) {
 		}
 	}
 	channel <- lines
+}
+
+func makeDistribution(file *mmap.File, threads int) ([]int64, error) {
+	if threads <= 0 {
+		return nil, fmt.Errorf("invalid value of threads")
+	}
+
+	if threads == 1 {
+		panic("TODO")
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+	size := info.Size()
+	distribution := make([]int64, threads+1)
+
+	var limit int64 = size / int64(threads)
+	for i := 0; i < len(distribution)-1; i++ {
+		distribution[i] = int64(i) * limit
+	}
+	// 0, 4, 8, 12, `17`
+	// 0:4, 4:8, 8:12, 12:17
+	distribution[len(distribution)-1] = size
+
+	for i := 1; i < len(distribution)-1; i++ {
+		// this relay on the assumption of lines been short and the range is larger
+		if file.At(int(distribution[i])-1) != '\n' {
+			dindex := distribution[i]
+			for {
+				if file.At(int(dindex)) == '\n' {
+					break
+				}
+				dindex++
+			}
+			distribution[i] = dindex + 1
+		}
+	}
+
+	return distribution, nil
 }
