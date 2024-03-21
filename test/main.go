fe1db8785fc35f41f1b81aa8fb35913ca613243e
ross96D <abelaross96@gmail.com>
2024-03-20 23:57:04 -0400

    some shits added

--- test/main.go
+++ test/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"hash/fnv"
 	"runtime"
 	"sync"
 	"time"
@@ -10,13 +11,12 @@ import (
 )
 
 type CityMetadata struct {
-	Min   int
-	Max   int
-	Sum   int
-	Count int
+	Name  string
+	Min   float64
+	Max   float64
+	Sum   float64
+	Count float64
 }
-
-var cityMap = sync.Map{}
 
 const filename = "../measurements.txt"
 
@@ -66,9 +66,15 @@ func main() {
 }
 
 func read(start int64, end int64, file *mmap.File, channel chan int64) {
+	var cityMap = map[uint64]CityMetadata{}
+
 	if end-start <= 0 {
 		return
 	}
+	if start != 0 && file.At(int(start)-1) != '\n' {
+		panic("distribution is wrong")
+	}
+
 	var cum int64 = 0
 	var lines int64 = 0
 	const length int64 = 4098
@@ -77,6 +83,7 @@ func read(start int64, end int64, file *mmap.File, channel chan int64) {
 	var bToRead int64
 	for offset < end {
 		// read bytes from file
+		// TODO this is eating one line because when it reads the first time the second read does not start after a new line
 		if length+offset > end {
 			bToRead = end - offset
 		} else {
@@ -89,13 +96,92 @@ func read(start int64, end int64, file *mmap.File, channel chan int64) {
 		offset += bToRead
 		cum += int64(n)
 
+		start := 0
 		for i := 0; i < int(bToRead); i++ {
+			if start != 0 && buff[start-1] != '\n' {
+				panic("WTF")
+			}
 			if buff[i] == '\n' {
-				lines++
+				var before byte = 2
+				if start != 0 {
+					before = buff[start-1]
+				}
+				parseLine(cityMap, buff[start:i], buff[i], before)
+				start = i + 1
 			}
 		}
 	}
 	channel <- lines
+}
+
+func parseLine(cityMap map[uint64]CityMetadata, line []byte, last byte, before byte) {
+	if last != '\n' {
+		panic("Z")
+	}
+	sep := 0
+	for i, b := range line {
+		if b == ';' {
+			sep = i
+			break
+		}
+	}
+	if sep == 0 {
+		return
+		panic(fmt.Sprintf("could not parse line %s  %d", string(line), before))
+	}
+
+	hasher := fnv.New64a()
+	hasher.Write(line[:sep-1])
+
+	cityName := hasher.Sum64()
+	// println(cityName, string(line[sep+1:]))
+	val := parseFloat(line[sep+1:])
+	// if err != nil {
+	// 	fmt.Println(string(string(line[sep+1:])))
+	// 	panic(err)
+	// }
+	cityData, ok := cityMap[cityName]
+	if !ok {
+		cityData = CityMetadata{
+			Name:  string(string(line[:sep-1])),
+			Min:   val,
+			Max:   val,
+			Sum:   val,
+			Count: 1,
+		}
+	} else {
+		cityData.Count += 1
+		cityData.Sum += val
+		if cityData.Max < val {
+			cityData.Max = val
+		} else if cityData.Min > val {
+			cityData.Min = val
+		}
+	}
+	cityMap[cityName] = cityData
+}
+
+func parseFloat(b []byte) float64 {
+	var result float64
+	isNegative := false
+	if b[0] == '-' {
+		isNegative = true
+		b = b[1:]
+	}
+	switch len(b) {
+	case 1, 2:
+		result = float64(b[0])
+	case 3:
+		result = float64(b[0]) + float64(b[2])*0.1
+	case 4:
+		result = float64(b[0]*10+b[1]) + float64(b[3])*0.1
+	default:
+		panic(fmt.Sprintf("SSSSSSSSSSSSSS %s", string(b)))
+	}
+	if isNegative {
+		result *= -1
+	}
+	return result
 }
 
 func makeDistribution(file *mmap.File, threads int) ([]int64, error) {
@@ -118,8 +204,7 @@ func makeDistribution(file *mmap.File, threads int) ([]int64, error) {
 	for i := 0; i < len(distribution)-1; i++ {
 		distribution[i] = int64(i) * limit
 	}
-	// 0, 4, 8, 12, `17`
-	// 0:4, 4:8, 8:12, 12:17
+
 	distribution[len(distribution)-1] = size
 
 	for i := 1; i < len(distribution)-1; i++ {
